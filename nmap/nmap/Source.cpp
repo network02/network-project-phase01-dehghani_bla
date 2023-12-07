b873f3c112106f6e2b583ddeafa4da3297680884
ironrust1 <gol.samen81@gmail.com>
2023-12-08 00:32:47 +0330

    basics coded and i go sleepping !

---
+++ nmap/nmap/Source.cpp
@@ -0,0 +1,17 @@
+#include<winsock.h>
+#include <stdio.h>
+
+// client program
+int main()
+{
+	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in serveraddr;
+	serveraddr
+	connect(clientSocket,
+	puts("\t\t\t in the name of GOD \t\t\t");
+	puts("1_ online test .");
+	puts("2_ port scanning .");
+	puts("3_ get and post http request .");
+	puts("please select one choices above and enter its number");
+	return (0);
+}
